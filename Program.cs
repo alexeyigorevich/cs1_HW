87842fc7e636e16c3a97d790cbdf58b7a786367b
alexeyigorevich <venom250394@gmail.com>
2023-04-14 10:28:29 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int a, b, c, result;
+
+
+Console.WriteLine("Введите номер задачи: ");
+switch (Console.ReadLine())
+{
+    case "1":
+        Console.WriteLine("Введите два числа");
+        if (int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b))
+        {
+            if (a > b)
+            {
+                Console.Write("Большее из них: " + a);
+            }
+            else
+            {
+                Console.Write("Большее из них: " + b);
+            }
+        }
+        else
+        {
+            Console.Write("Вы ввели неверные значения");
+        }
+        break;
+
+}
+fsdfsd
